7645cb7d057638905b48838a5325b9440f751b8f
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2025-09-29 21:48:19 -0400

    chore: add copyright headers to relevant files

--- src/main/java/dev/jaxydog/ptm/mixin/client/GameOptionsMixin.java
+++ src/main/java/dev/jaxydog/ptm/mixin/client/GameOptionsMixin.java
@@ -1,3 +1,22 @@
+/*
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * Copyright © 2025 Jaxydog
+ *
+ * This file is part of Permission to Maim.
+ *
+ * Permission to Maim is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Permission to Maim is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with Permission to Maim. If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
 package dev.jaxydog.ptm.mixin.client;
 
 import dev.jaxydog.ptm.PermissionToMaim;
