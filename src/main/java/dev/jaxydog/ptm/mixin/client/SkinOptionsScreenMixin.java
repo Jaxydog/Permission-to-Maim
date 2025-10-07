69efce058af3bfbf6eb06d1dfae8c0a8f2de7a46
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2025-10-07 19:59:08 -0400

    fix: implement proper section headers

--- src/main/java/dev/jaxydog/ptm/mixin/client/SkinOptionsScreenMixin.java
+++ src/main/java/dev/jaxydog/ptm/mixin/client/SkinOptionsScreenMixin.java
@@ -24,22 +24,26 @@ import dev.jaxydog.ptm.inject.client.PtmGameOptions;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
 import net.minecraft.client.gui.screen.option.SkinOptionsScreen;
 import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.client.gui.widget.CyclingButtonWidget;
+import net.minecraft.client.gui.widget.OptionListWidget;
 import net.minecraft.client.gui.widget.TextWidget;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.function.IntSupplier;
 
 @Environment(EnvType.CLIENT)
 @Mixin(SkinOptionsScreen.class)
@@ -56,12 +60,39 @@ public abstract class SkinOptionsScreenMixin
         super(parent, gameOptions, title);
     }
 
+    @Unique
+    private @NotNull OptionListWidget.WidgetEntry createHeader(final @NotNull Text text) {
+        final IntSupplier bodyWidth = () -> Objects.requireNonNull(this.body).getWidth();
+
+        return new OptionListWidget.WidgetEntry(List.of(new TextWidget(text, this.textRenderer)), this) {
+
+            @Override
+            public void render(
+                final @NotNull DrawContext context,
+                final int mouseX,
+                final int mouseY,
+                final boolean hovered,
+                final float deltaTicks
+            )
+            {
+                final @NotNull TextWidget widget = (TextWidget) this.widgets.getFirst();
+
+                widget.setHeight(21);
+                widget.setPosition((bodyWidth.getAsInt() / 2) - (widget.getWidth() / 2), this.getContentY());
+                widget.render(context, mouseX, mouseY, deltaTicks);
+            }
+        };
+    }
+
+    @Inject(method = "addOptions", at = @At("HEAD"))
+    private void addOptions_addVanillaHeader(final @NotNull CallbackInfo ci) {
+        Objects.requireNonNull(this.body).addEntry(this.createHeader(Text.translatable("options.ptm.vanillaTitle")));
+    }
+
     @SuppressWarnings("RedundantCast")
     @Inject(method = "addOptions", at = @At("TAIL"))
     private void addOptions_addPtmButtons(final @NotNull CallbackInfo ci) {
-        Objects.requireNonNull(this.body).addAll(List.of(
-            new TextWidget((150 * 2) + 10, 20, Text.translatable("options.ptm.title"), this.textRenderer)
-        ));
+        Objects.requireNonNull(this.body).addEntry(this.createHeader(Text.translatable("options.ptm.title")));
 
         final @NotNull PtmGameOptions ptmGameOptions = (PtmGameOptions) this.gameOptions;
         final @NotNull List<ClickableWidget> widgetList = new ObjectArrayList<>(PtmModelPart.values().length);
